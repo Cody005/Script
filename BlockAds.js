2b54a7e1bfc87f158580a51a110b2098a1d3b857
Cody005 <72626458+Cody005@users.noreply.github.com>
2023-10-28 20:36:59 +0200

    Update BlockAds.js

--- BlockAds.js
+++ BlockAds.js
@@ -3,7 +3,7 @@ Membership unlock for VSCO & 1Blocker & HTTPBot & Grindy & Scruffy
 Please note that you may need to reinstall app for script to work.
 
 QuantumultX rewrite link:
-https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/vsco.js
+https://raw.githubusercontent.com/Cody005/Script/main/BlockAds.js
 
 Please note that the above rewrite link requires open KOP-XIAO's resource parser
 
@@ -11,8 +11,8 @@ Please note that the above rewrite link requires open KOP-XIAO's resource parser
 Surge4, Loon and Shadowrocket configuration:
 
 [Script]
-http-request ^https?:\/\/api\.revenuecat\.com\/v\d\/subscribers\/ script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/vsco.js
-http-response ^https?:\/\/api\.revenuecat\.com\/v\d\/subscribers\/ requires-body=1,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/vsco.js
+http-request ^https?:\/\/api\.revenuecat\.com\/v\d\/subscribers\/ script-path=https://raw.githubusercontent.com/Cody005/Script/main/BlockAds.js
+http-response ^https?:\/\/api\.revenuecat\.com\/v\d\/subscribers\/ requires-body=1,script-path=https://raw.githubusercontent.com/Cody005/Script/main/BlockAds.js
 
 [MITM]
 hostname = api.revenuecat.com
