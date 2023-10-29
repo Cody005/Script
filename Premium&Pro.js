63ae6ae2f189f6b71c786e3acc4872ccb88324b4
Cody005 <72626458+Cody005@users.noreply.github.com>
2023-10-29 22:24:58 +0100

    Update Premium&Pro.js

--- Premium&Pro.js
+++ Premium&Pro.js
@@ -10,7 +10,7 @@
 hostname = pxxxqh-launches.appsflyersdk.com
 
 [rewrite_local]
-^https?:\/\/pxxxqh-launches\.appsflyersdk\.com\/?  script-response-body https://raw.githubusercontent.com/Cody005/Script/main/Premium%26Pro.js
+^https?:\/\/pxxxqh-launches\.appsflyersdk\.com\/? script-response-body https://raw.githubusercontent.com/Cody005/Script/main/Premium%26Pro.js?token=GHSAT0AAAAAACJRWXSC4ZARZM5WZHHTGN2OZJ6ZXGA
 
 ******************************************/
 const body = $response.body
